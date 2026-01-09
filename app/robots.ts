1c99d2e55f6f865d5805282c9b02bd488fec745d
arafat <anmrfat8@gmail.com>
2026-01-09 20:25:17 +0300

    إضافة الهيدر والفوتر ونظام الكلمات المفتاحية وتحسين ضغط الصور

--- app/robots.ts
+++ app/robots.ts
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
       disallow: ['/admin', '/admin/*', '/login'],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
-    host: baseUrl,
+    
   };
 }
